63d90878bbf17fe0c15ba9d6d086abb9ccd6e4ab
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2023-12-15 11:27:16 -0300

    Relógio Analógico pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const relogio= setInterval(function time(){
     let hr= dateToday.getHours();
     let mn= dateToday.getMinutes();
     let sc= dateToday.getSeconds();
-
+// Relogio digital
     if(hr<10){
         hr = '0' + hr;
     }
@@ -17,8 +17,23 @@ const relogio= setInterval(function time(){
     if(sc<10){
         sc = '0' + sc;
     }
-
     horas.textContent = hr;
     minutos.textContent = mn;
     segundos.textContent = sc;
+
+    //Relógio Analógico
+    hrValue = (hr * 30) + (mn * 6)/12;
+    mnValue = mn * 6;
+    scValue = sc * 6;
+
+    document.querySelector(".hr-hand").style.transform = 
+    "rotate(" + hrValue + "deg)";
+   
+    document.querySelector(".min-hand").style.transform = 
+    "rotate(" + mnValue + "deg)";
+  
+    document.querySelector(".sec-hand").style.transform = 
+    "rotate(" + scValue + "deg)";
+
+
 })
